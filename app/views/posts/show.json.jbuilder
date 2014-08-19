1067be8d0b470e96398af8bc837bfece68cb16b3
RolandStilting <rolandstilting@Rolands-MacBook-Pro.local>
2014-08-19 15:38:13 +0200

    initial commit3

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :user, :text, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725115549) do
+ActiveRecord::Schema.define(version: 20140819133615) do
+
+  create_table "posts", force: true do |t|
+    t.string   "user"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title"
